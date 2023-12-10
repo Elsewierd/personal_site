066b392a99ad17eea86b5112c05e3aea96789de6
Dean Freeman <dean.freemanwilde@gmail.com>
2023-12-10 13:23:33 -0500

    created templates for /blog and connected the urls

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Catagory, Comment, Post
 
-# Register your models here.
+class CatagoryAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Catagory, CatagoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post, PostAdmin)
--- personal_site/settings.py
+++ personal_site/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Project Specific Apps
-    'blog.apps.BlogConfig',
+    'blog.apps',
 ]
 
 MIDDLEWARE = [
